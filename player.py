614808d3ee0aa202888db2f26a20dcdd856c844c
Space-Station1 <david.kroken2@gmail.com>
2026-03-09 18:07:22 +0100

    Update player.py

--- player.py
+++ player.py
@@ -5,23 +5,13 @@ class Player:
         self.health = 100
         self.level = 1
         self.xp = 0
-        self.inventory = []
+        self.inventory = [] # Her lagres gjenstandene dine
+
+    def add_item(self, item_name):
+        self.inventory.append(item_name)
+        print(f"You picked up: {item_name}")
 
     def status(self):
-        return f"Name: {self.name} | Level: {self.level} | XP: {self.xp} | Health: {self.health}"
+        return f"Name: {self.name} | Lvl: {self.level} | Health: {self.health} | Inventory: {self.inventory}"
 
-    def gain_xp(self, amount):
-        self.xp += amount
-        print(f"You gained {amount} XP!")
-        if self.xp >= 50:
-            self.level_up()
-
-    def level_up(self):
-        self.level += 1
-        self.xp = 0
-        self.health += 20
-        print(f"--- CONGRATULATIONS! You reached level {self.level}! Health increased to {self.health}. ---")
-
-    def take_damage(self, amount):
-        self.health -= amount
-        print(f"You took {amount} damage! Health is now: {self.health}.")
+    # ... behold gain_xp, level_up og take_damage slik de var
